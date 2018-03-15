daa5b35d2d939ec41871cdf056abcb175e58a366
(Amanda Orbe) <garayomiriam@gmail.com>
2018-03-15 16:21:35 +0100

    tabs

--- seom/source/javascripts/application.js
+++ seom/source/javascripts/application.js
@@ -1,3 +1,12 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+$(function() {
+
+  $('.tabs a').on('click', function(){
+      $('.tabs a.active').removeClass('active', 400);
+      $(this).addClass('active', 400);
+  });
+});
+
