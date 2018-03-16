a094d68186808f59b312f34a5cdbb8723eb7d231
(Amanda Orbe) <garayomiriam@gmail.com>
2018-03-16 14:29:03 +0100

    form

--- seom/source/javascripts/application.js
+++ seom/source/javascripts/application.js
@@ -5,8 +5,19 @@
 $(function() {
 
   $('.tabs a').on('click', function(){
-      $('.tabs a.active').removeClass('active', 400);
-      $(this).addClass('active', 400);
+      $('.tabs a.active').removeClass('active', 600);
+      $(this).addClass('active', 600);
+
+  });
+
+  $('.btn-navbar').on('click', function(){
+      $('.lightbox').fadeIn(500);
+      $('body').css("overflow", "hidden");
+
+  });
+   $('.btn-cancel').on('click', function(){
+      $('.lightbox').fadeOut(500);
+      $('body').css("overflow", "initial");
   });
 });
 
